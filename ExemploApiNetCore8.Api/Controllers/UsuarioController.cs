0244be48483854525867c9ab1b3d3ab0cf60389a
Daniel Scharp <daniel.scharp@hotmail.com>
2024-02-21 15:33:56 -0300

    Início de uma API com .net8

---
+++ ExemploApiNetCore8.Api/Controllers/UsuarioController.cs
@@ -0,0 +1,32 @@
+﻿using ExemploApiNetCore8.App;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExemploApiNetCore8.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        private readonly UsuarioApplication _usuarioApplication;
+
+        public UsuarioController(UsuarioApplication usuarioApplication)
+        {
+            _usuarioApplication = usuarioApplication;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Listar()
+        {
+            try
+            {
+                var listaUsuarios = await _usuarioApplication.ListarUsuarios();
+                return Ok(listaUsuarios);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
